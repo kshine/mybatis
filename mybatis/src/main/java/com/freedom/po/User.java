057b60ea8925e958e86371aec858ff5b728f98c7
kshine <250270704@qq.com>
2017-08-19 15:58:18 +0800

    添加教程项目 mybatis

---
+++ mybatis/src/main/java/com/freedom/po/User.java
@@ -0,0 +1,62 @@
+package com.freedom.po;
+
+import java.util.Date;
+
+public class User {
+    private int id;
+    private String userName;
+    private String sex;
+    private Date birthday;
+    private String address;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", sex='" + sex + '\'' +
+                ", birthday=" + birthday +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
